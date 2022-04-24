1de7c7d2fbe4731f34d105551b05e95b370489a7
lvkgamer6307 <90861934+lvkgamer6307@users.noreply.github.com>
2022-04-24 14:35:33 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,23 +1,29 @@
+input.onButtonPressed(Button.AB, function () {
+    alles = 0
+})
 radio.onReceivedValue(function (name, value) {
-    if (name.compare("mgx") == 0) {
-        rechts = value
+    if (alles == 69) {
+        if (name.compare("mgx") == 0) {
+            rechts = value
+        }
+        if (name.compare("mgy") == 0) {
+            achterui = value
+        }
+        if (name.compare("mgz") == 0) {
+            achterui = value
+        }
+        r_motor = -1 * achterui - rechts
+        l_motor = -1 * achterui - rechts
     }
-    if (name.compare("mgy") == 0) {
-        achterui = value
-    }
-    if (name.compare("mgz") == 0) {
-        achterui = value
-    }
-    r_motor = -1 * achterui - rechts
-    l_motor = -1 * achterui - rechts
 })
 let l_motor = 0
 let r_motor = 0
 let achterui = 0
 let rechts = 0
+let alles = 0
+alles = 69
 radio.setGroup(69)
 basic.forever(function () {
-    let b_motor = 0
     if (r_motor > 0) {
         pins.analogWritePin(AnalogPin.P0, 0)
         pins.analogWritePin(AnalogPin.P1, Math.abs(r_motor))
@@ -32,11 +38,21 @@ basic.forever(function () {
         pins.analogWritePin(AnalogPin.P10, 0)
         pins.analogWritePin(AnalogPin.P4, Math.abs(l_motor))
     }
-    if (b_motor > 0) {
-        pins.analogWritePin(AnalogPin.P2, 0)
-        pins.analogWritePin(AnalogPin.P3, Math.abs(b_motor))
-    } else {
-        pins.analogWritePin(AnalogPin.P3, 0)
-        pins.analogWritePin(AnalogPin.P2, Math.abs(b_motor))
+    while (alles == 0) {
+        let b_motor = 0
+        if (b_motor > 0) {
+            pins.analogWritePin(AnalogPin.P2, 0)
+            pins.analogWritePin(AnalogPin.P3, Math.abs(b_motor))
+        } else {
+            pins.analogWritePin(AnalogPin.P3, 0)
+            pins.analogWritePin(AnalogPin.P2, Math.abs(b_motor))
+        }
+    }
+    if (alles == 0) {
+        if (input.buttonIsPressed(Button.A)) {
+            alles = 60
+            pins.analogWritePin(AnalogPin.P0, 1023)
+            pins.analogWritePin(AnalogPin.P0, 1023)
+        }
     }
 })
